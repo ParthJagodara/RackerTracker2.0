d8b0918123501feff11a531295daea68692d9977
KevinPike <kevin.e.pike@gmail.com>
2012-07-13 02:03:46 -0400

    stuff for query racker admin page

--- RackerTrackerX/rackertracker/adminpages.py
+++ RackerTrackerX/rackertracker/adminpages.py
@@ -4,3 +4,6 @@ from rackertracker.workouthelper import getPercentage
 
 def selectwinner(request):
     return render_to_response("adminselectwinner.html", RequestContext(request, {'list': getPercentage()}))
+
+def queryRacker(request):
+    return render_to_response("adminqueryracker.html")
